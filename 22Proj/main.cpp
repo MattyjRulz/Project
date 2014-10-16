5f88d85e65184876b967eba0bb81a193eafdcb86
MattyjRulz <matthewdavidjones@mdj.local>
2014-10-17 10:26:50 +1100

    Initial Commit

---
+++ 22Proj/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  22Proj
+//
+//  Created by MattyjRulz on 17/10/2014.
+//  Copyright (c) 2014 mdj266. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
